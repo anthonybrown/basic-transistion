2cea93acb54c3ec37e55dcd98160394c53577627
Tony Brown <tony@tonybrown.me>
2019-02-19 06:10:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function App() {
   })
 
   return (
-    <div {...bind} class="list" style={{ height: Math.max(...heights) }}>
+    <div {...bind} className="list" style={{ height: Math.max(...heights) }}>
       {transitions.map(({ item, props: { xy, ...rest }, key }) => (
         <a.div key={key} style={{ transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`), ...rest }}>
           <div style={{ backgroundImage: item.css }} />
